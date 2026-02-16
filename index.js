8c98ea3d805171163a25185af042d3452f85458b
Filippo-Angaroni <96851393+Filippo-Angaroni@users.noreply.github.com>
2026-02-16 18:22:48 +0100

    Footer e Header file

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 /* Questa è un riga di test scritta da Elisa per far vedere ad Alessia come funziona Git */
+
+if (window.location.pathname === '/index.html') {
+
+    fetch('./header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        });
+
+    fetch('./footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        });
+} else {
+    fetch('../header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        });
+
+    fetch('../footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        });
+
+    }
+    
+console.log(window.location.pathname);
